16ae69440f8266d2c8eead454e98e73a1ba874dc
Daniel Sánchez <ingdanielsanchezve@gmail.com>
2019-05-28 23:14:45 -0400

    Recommendations

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -2,7 +2,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col">
-							<h2 class="text-color-quaternary font-weight-extra-bold text-uppercase">Recommendations</h2>
+							<h2 class="text-color-light font-weight-extra-bold text-uppercase">Recommendations</h2>
 						</div>
 						<div class="col-lg-12 p-0">
 							<div class="owl-carousel custom-dots-style-1 custom-dots-color-1 custom-dots-position-1 mb-0" data-plugin-options="{'items': 1, 'autoHeight': true, 'loop': false, 'nav': false, 'dots': true}">
@@ -10,10 +10,10 @@
 									<div class="col">
 										<div class="testimonial custom-testimonial-style-1 testimonial-with-quotes mb-0">
 											<blockquote class="pb-2">
-												<p class="custom-text-color-3">I had the chance to interview Daniel as part of a selection process and I immediately had a very good impression about him. Daniel is a very proactive professional with solid work experience and a great personal attitude.</p>
+												<p class="custom-text-color-4">I had the chance to interview Daniel as part of a selection process and I immediately had a very good impression about him. Daniel is a very proactive professional with solid work experience and a great personal attitude.</p>
 											</blockquote>
 											<div class="testimonial-author float-left">
-												<p><strong class="text-uppercase">Gabriele Murrone</strong><span class="custom-text-color-3">CEO & Co-founder at ifeel</span></p>
+												<p><strong class="text-uppercase">Gabriele Murrone</strong><span class="custom-text-color-4">CEO & Co-founder at ifeel</span></p>
 											</div>
 										</div>
 									</div>
@@ -22,10 +22,10 @@
 									<div class="col">
 										<div class="testimonial custom-testimonial-style-1 testimonial-with-quotes mb-0">
 											<blockquote class="pb-2">
-												<p class="custom-text-color-3">Understood complicated business rules and completed the projects.</p>
+												<p class="custom-text-color-4">Understood complicated business rules and completed the projects.</p>
 											</blockquote>
 											<div class="testimonial-author float-left">
-												<p><strong class="text-uppercase">Keith Lubel</strong><span class="custom-text-color-3">CTO at Berkery noyes</span></p>
+												<p><strong class="text-uppercase">Keith Lubel</strong><span class="custom-text-color-4">CTO at Berkery noyes</span></p>
 											</div>
 										</div>
 									</div>
