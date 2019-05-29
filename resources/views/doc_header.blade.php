7f674f434a2f81776f03c8d8601dd07433e29c83
Daniel Sánchez <ingdanielsanchezve@gmail.com>
2019-05-28 21:14:25 -0400

    favicon2

--- resources/views/doc_header.blade.php
+++ resources/views/doc_header.blade.php
@@ -2,9 +2,9 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    		<title>Daniel Sánchez - Full Stack Web Developer - PHP / Javascript / Angular / VueJs / MySQL / Oracle / MongoDB / Linux / AWS / Docker</title>	
+    	<title>Daniel Sánchez - Full Stack Web Developer - PHP / Javascript / Laravel / NodeJs / Angular / VueJs / MySQL / Oracle / MongoDB / Linux / AWS / Docker</title>	
 
-		<meta name="keywords" content="Daniel Sánchez" /> 
+		<meta name="keywords" content="Daniel Sánchez - Full Stack Web Developer - PHP / Javascript / Laravel / NodeJs / Angular / VueJs / MySQL / Oracle / MongoDB / Linux / AWS / Docker" /> 
 
 		<meta name="description" content="Daniel Sánchez Computer engineer with a large web development and software engineering experience using LAMP and MEAN stacks, knowledge in responsive web site development, HTML5, CSS3, Bootstrap, Javascript, Jquery, Ajax, AngularJS, BackboneJS, Underscore, Mustache, GruntJs, VueJs,NodeJs, MochaJs, ChaiJs, PHP, Laravel Framework, SQL, MySQL, Oracle, PostgreSQL, Firebird, MongoDB, DynamoDB Databases, Linux, GIT, SVN, Facebook, Twitter, Instagram, LinkedIn, Pinterest, Mailchimp and PayU developers APIs, Web Services and RESTful APIs development, Test Driven Development TDD, Unit Testing, MVC software architecture pattern, Apache module implementation, Virtual Servers administration on Amazon Web Services AWS EC2 instances, Linux Server Administration (Debian, Ubuntu, CentOS), SEO knowledges, Pl-SQL, Application Development with Oracle Developer Suite 6, Visual Studio 6, Agile development methodologies XP and SCRUM, Docker Images and containers">
 
@@ -23,7 +23,7 @@
 	     <meta property="og:type"        content="website" />
 	     <meta property="og:title"       content="Daniel Sánchez" />
 	     <meta property="og:description" content="Daniel Sánchez Computer engineer with a large web development and software engineering experience using LAMP and MEAN stacks, knowledge in responsive web site development, HTML5, CSS3, Bootstrap, Javascript, Jquery, Ajax, AngularJS, BackboneJS, Underscore, Mustache, GruntJs, VueJs,NodeJs, MochaJs, ChaiJs, PHP, Laravel Framework, SQL, MySQL, Oracle, PostgreSQL, Firebird, MongoDB, DynamoDB Databases, Linux, GIT, SVN, Facebook, Twitter, Instagram, LinkedIn, Pinterest, Mailchimp and PayU developers APIs, Web Services and RESTful APIs development, Test Driven Development TDD, Unit Testing, MVC software architecture pattern, Apache module implementation, Virtual Servers administration on Amazon Web Services AWS EC2 instances, Linux Server Administration (Debian, Ubuntu, CentOS), SEO knowledges, Pl-SQL, Application Development with Oracle Developer Suite 6, Visual Studio 6, Agile development methodologies XP and SCRUM, Docker Images and containers" />
-	     <meta property="og:image"       content="{{ asset('img/favicon.png') }}" />
+	     <meta property="og:image"       content="{{ asset('img/favicon/favicon.png') }}" />
 	     <meta property="og:locale:alternate" content="es" />
 
 	     <meta name="author" content="Daniel Sánchez">
@@ -67,25 +67,25 @@
 	    <link rel="alternate" href="https://iamdanielsanchez.ml/es/" hreflang="es">
 
 		<!-- Favicon -->
-		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png" />
-		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png" />
-		<link rel="icon" type="image/png" href="img/favicon/favicon-196x196.png" sizes="196x196" />
-		<link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96" />
-		<link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32" />
-		<link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16" />
-		<link rel="icon" type="image/png" href="img/favicon/favicon-128.png" sizes="128x128" />
-		<meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png" />
-		<meta name="msapplication-square70x70logo" content="img/favicon/mstile-70x70.png" />
-		<meta name="msapplication-square150x150logo" content="img/favicon/mstile-150x150.png" />
-		<meta name="msapplication-wide310x150logo" content="img/favicon/mstile-310x150.png" />
-		<meta name="msapplication-square310x310logo" content="img/favicon/mstile-310x310.png" />
-		<meta name="application-name" content="&nbsp;"/>
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('img/favicon/apple-touch-icon-57x57.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('img/favicon/apple-touch-icon-114x114.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('img/favicon/apple-touch-icon-72x72.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('img/favicon/apple-touch-icon-144x144.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('img/favicon/apple-touch-icon-60x60.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('img/favicon/apple-touch-icon-120x120.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('img/favicon/apple-touch-icon-76x76.png') }}" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('img/favicon/apple-touch-icon-152x152.png') }}" />
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon/favicon-196x196.png') }}" sizes="196x196" />
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon/favicon-96x96.png') }}" sizes="96x96" />
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon/favicon-32x32.png') }}" sizes="32x32" />
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon/favicon-16x16.png') }}" sizes="16x16" />
+		<link rel="icon" type="image/png" href="{{ asset('img/favicon/favicon-128.png') }}" sizes="128x128" />
+		<meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png') }}" />
+		<meta name="msapplication-square70x70logo" content="{{ asset('img/favicon/mstile-70x70.png') }}" />
+		<meta name="msapplication-square150x150logo" content="{{ asset('img/favicon/mstile-150x150.png') }}" />
+		<meta name="msapplication-wide310x150logo" content="{{ asset('img/favicon/mstile-310x150.png') }}" />
+		<meta name="msapplication-square310x310logo" content="{{ asset('img/favicon/mstile-310x310.png') }}" />
+		<meta name="application-name" content="Daniel Sánchez "/>
 		<meta name="msapplication-TileColor" content="#FFFFFF" />
 
 
